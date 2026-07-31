7f08146468883c1176c201a097dc5268fa00f366
Prashna Raut <prashnaraut32@gmail.com>
2026-07-31 14:23:43 +0545

    12 th commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div style={{ padding: '4rem 2rem', textAlign: 'center' }}>
+      <h1>Welcome to EventNova</h1>
+      <p style={{ marginTop: '1rem', color: 'var(--text-secondary)' }}>
+        Your modern platform for discovering events. (Hero Section Coming in Day 2)
+      </p>
+    </div>
+  );
+};
+
+export default Home;
