f306b88071f4768786feee2a7f381afc1e91e0d7
Prashna Raut <prashnaraut32@gmail.com>
2026-08-01 09:09:51 +0545

    27th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import Home from './pages/Home/Home';
+import Events from './pages/Events/Events'; // New import
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="events" element={<Events />} /> {/* New route */}
           {/* Future routes will go here */}
         </Route>
       </Routes>
